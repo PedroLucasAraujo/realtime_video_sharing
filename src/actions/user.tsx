cd52ee45ae0744f54e0847ed9f89bcb461c0e14d
pedroLucasArauj0 <pedrolucas.araujo@cadastra.com>
2024-11-20 15:24:40 -0300

    feat: create prisma schema

---
+++ src/actions/user.tsx
@@ -0,0 +1,19 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+export const onAuthenticateUser = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return { status: 403 };
+    }
+
+    const existingUser = await db.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+    });
+  } catch {}
+};
