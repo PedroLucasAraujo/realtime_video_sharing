a924051c9aebe775d957a70a29343b2dedf1779e
pedroLucasArauj0 <pedrolucas.araujo@cadastra.com>
2024-11-20 15:36:31 -0300

    feat: create authentate user action

--- src/actions/user.tsx
+++ src/actions/user.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { db } from "@/lib/prisma";
+import { client } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 
 export const onAuthenticateUser = async () => {
@@ -10,10 +10,73 @@ export const onAuthenticateUser = async () => {
       return { status: 403 };
     }
 
-    const existingUser = await db.user.findUnique({
+    const userExist = await client.user.findUnique({
       where: {
-        clerkId: user.id,
+        clerkid: user.id,
+      },
+      include: {
+        workspace: {
+          where: {
+            User: {
+              clerkid: user.id,
+            },
+          },
+        },
       },
     });
-  } catch {}
+
+    if (userExist) {
+      return {
+        status: 200,
+        user: userExist,
+      };
+    }
+
+    const newUser = await client.user.create({
+      data: {
+        clerkid: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        firstname: user.firstName,
+        lastname: user.lastName,
+        image: user.imageUrl,
+        studio: {
+          create: {},
+        },
+        subscription: {
+          create: {},
+        },
+        workspace: {
+          create: {
+            name: `${user.firstName}'s Workspace`,
+            type: "PERSONAL",
+          },
+        },
+      },
+      include: {
+        workspace: {
+          where: {
+            User: {
+              clerkid: user.id,
+            },
+          },
+        },
+        subscription: {
+          select: {
+            plan: true,
+          },
+        },
+      },
+    });
+
+    if (newUser) {
+      return {
+        status: 201,
+        user: newUser,
+      };
+    }
+
+    return { status: 400 };
+  } catch {
+    return { status: 500 };
+  }
 };
